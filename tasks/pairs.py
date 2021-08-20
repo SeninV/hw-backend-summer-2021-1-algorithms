17fa85e7ea5efcc14b8d8576d4235811016e5989
vladimir <v@senin.xyz>
2021-08-21 00:54:59 +0300

    pairs even_odd prime timestr

--- tasks/pairs.py
+++ tasks/pairs.py
@@ -4,10 +4,19 @@ __all__ = (
     'corresponding_pairs',
 )
 
+ # ([1, 2, 5, 6], [3, 4], [(1, 3), (2, 4)])
+
 
 def corresponding_pairs(arr1: list[Any], arr2: list[Any]) -> list[tuple[Any, Any]]:
+    dlina = min(len(arr1), len(arr2))
+    slov = []
+    for i in range(dlina):
+        slov.append(tuple([arr1[i], arr2[i]]))
+
     """
     Функция должна возвращать соответствующие элементы двух массивов:
     corresponding_pairs([1, 2], [3, 4]) == [(1, 3), (2, 4)]
-    """
-    raise NotImplementedError
+     """
+    return slov
+
+print(corresponding_pairs([1, 2, 3], [3, 4]))
