3e8cf1a29980b2eaf73a585bdd8c014c32c820a4
vladimir <v@senin.xyz>
2021-08-21 00:56:43 +0300

    pairs

--- tasks/pairs.py
+++ tasks/pairs.py
@@ -4,13 +4,10 @@ __all__ = (
     'corresponding_pairs',
 )
 
- # ([1, 2, 5, 6], [3, 4], [(1, 3), (2, 4)])
-
 
 def corresponding_pairs(arr1: list[Any], arr2: list[Any]) -> list[tuple[Any, Any]]:
-    dlina = min(len(arr1), len(arr2))
     slov = []
-    for i in range(dlina):
+    for i in range(min(len(arr1), len(arr2))):
         slov.append(tuple([arr1[i], arr2[i]]))
 
     """
@@ -18,5 +15,3 @@ def corresponding_pairs(arr1: list[Any], arr2: list[Any]) -> list[tuple[Any, Any
     corresponding_pairs([1, 2], [3, 4]) == [(1, 3), (2, 4)]
      """
     return slov
-
-print(corresponding_pairs([1, 2, 3], [3, 4]))
