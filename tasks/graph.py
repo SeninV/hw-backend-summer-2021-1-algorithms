8cb342d6ac72b1ab04a1895f88771f8393ca5f2c
vladimir <v@senin.xyz>
2021-08-22 00:08:17 +0300

    bfs (all)

--- tasks/graph.py
+++ tasks/graph.py
@@ -1,4 +1,6 @@
 from typing import Any
+from collections import deque
+
 
 __all__ = (
     'Node',
@@ -48,46 +50,18 @@ class Graph:
 
 
     def bfs(self) -> list[Node]:
-        return None
+        a = self._root
+        visited = []
+        visited.append(a)
+        stack = deque([a])
+        while stack:
+            sosed = stack.popleft()
+            for neighbour in sosed.outbound:
+                if neighbour not in set(visited):
+                    print(visited)
+                    visited.append(neighbour)
+                    stack.append(neighbour)
 
+        return visited
 
-
-a = Node('a')
-b = Node('b')
-c = Node('c')
-a.point_to(b)
-b.point_to(c)
-a.point_to(c)
-
-# a = Node('a')
-# b = Node('b')
-# c = Node('c')
-# d = Node('d')
-# e = Node('e')
-# f = Node('f')
-# g = Node('g')
-# h = Node('h')
-# i = Node('i')
-# k = Node('k')
-# a.point_to(b)
-# b.point_to(c)
-# c.point_to(d)
-# d.point_to(a)
-# b.point_to(d)
-# a.point_to(e)
-# e.point_to(f)
-# e.point_to(g)
-# f.point_to(i)
-# f.point_to(h)
-# g.point_to(k)
-
-g = Graph(a)
-print(g.dfs())
-
-
-        # assert dfs_values(g) == ['a', 'b', 'c']
-
-e = {1 , 3 , 4}
-
-q = {2, 5, 1}
 
