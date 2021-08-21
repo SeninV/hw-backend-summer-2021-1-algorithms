1db053cfff7f76381dfd04943729c1f2586a69b3
vladimir <v@senin.xyz>
2021-08-21 23:45:31 +0300

    dfs

--- tasks/graph.py
+++ tasks/graph.py
@@ -28,7 +28,66 @@ class Graph:
         self._root = root
 
     def dfs(self) -> list[Node]:
-        raise NotImplementedError
+        a = self._root
+        visited = [a]
+
+        def dfs_node(point):
+            x = point.outbound
+            for i in x:
+                visited.append(i)
+                if set(i.outbound) - set(visited) != set():
+                    dfs_node(i)
+            return None
+        dfs_node(a)
+        otv = []
+        for i in visited:
+            if i not in otv:
+                otv.append(i)
+
+        return otv
+
 
     def bfs(self) -> list[Node]:
-        raise NotImplementedError
+        return None
+
+
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+a.point_to(b)
+b.point_to(c)
+a.point_to(c)
+
+# a = Node('a')
+# b = Node('b')
+# c = Node('c')
+# d = Node('d')
+# e = Node('e')
+# f = Node('f')
+# g = Node('g')
+# h = Node('h')
+# i = Node('i')
+# k = Node('k')
+# a.point_to(b)
+# b.point_to(c)
+# c.point_to(d)
+# d.point_to(a)
+# b.point_to(d)
+# a.point_to(e)
+# e.point_to(f)
+# e.point_to(g)
+# f.point_to(i)
+# f.point_to(h)
+# g.point_to(k)
+
+g = Graph(a)
+print(g.dfs())
+
+
+        # assert dfs_values(g) == ['a', 'b', 'c']
+
+e = {1 , 3 , 4}
+
+q = {2, 5, 1}
+
